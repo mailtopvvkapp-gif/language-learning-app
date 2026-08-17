c895b1f55f89b5bce6faf5235bfaee2e9bb09f4f
mailtopvvkapp-gif <mailtopvvkapp@gmail.com>
2026-08-17 18:47:25 +0530

    Create audio_service.dart

---
+++ lib/core/audio_service.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+class AudioFeedbackService {
+  final FlutterTts _tts = FlutterTts();
+
+  Future<void> initTTS(String languageCode) async {
+    // తెలుగు కోసం: 'te-IN', హిందీ కోసం: 'hi-IN', ఇంగ్లీష్ కోసం: 'en-US'
+    await _tts.setLanguage(languageCode);
+    await _tts.setPitch(1.0);
+    await _tts.setSpeechRate(0.8);
+  }
+
+  Future<void> explainError({
+    required String customMessage,
+  }) async {
+    await _tts.stop();
+    await _tts.speak(customMessage);
+  }
+}
