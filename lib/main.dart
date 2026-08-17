e57a37b3330f163425a55fd9c66f75eed22e2252
mailtopvvkapp-gif <mailtopvvkapp@gmail.com>
2026-08-17 19:12:27 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_learning_app/core/audio_service.dart';
+import 'package:flutter_learning_app/core/language_provider.dart';
+import 'package:flutter_learning_app/modules/language_select/language_select_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => LanguageProvider(
+            audioService: AudioFeedbackService(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Learn Telugu, Hindi & English',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'NotoSansTelugu',
+          primarySwatch: Colors.deepPurple,
+        ),
+        home: const LanguageSelectScreen(),
+      ),
+    );
+  }
+}
