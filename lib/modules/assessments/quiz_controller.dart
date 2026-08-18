819b49c150ddf9348f7c8c715305dd27bf83e3b9
mailtopvvkapp-gif <mailtopvvkapp@gmail.com>
2026-08-18 09:58:20 +0530

    Update quiz_controller.dart

--- lib/modules/assessments/quiz_controller.dart
+++ lib/modules/assessments/quiz_controller.dart
@@ -21,11 +21,7 @@ class QuizQuestion {
   final List<String> options;
   final String correctAnswer;
 
-  QuizQuestion({
-    required this.item,
-    required this.options,
-    required this.correctAnswer,
-  });
+  QuizQuestion({required this.item, required this.options, required this.correctAnswer});
 }
 
 class AssessmentController {
@@ -38,38 +34,23 @@ class AssessmentController {
   }
 
   void _resetOrShufflePool() {
-    if (_usedItemIds.length >= allItems.length) {
-      _usedItemIds.clear();
-    }
-    _currentSessionPool = allItems
-        .where((item) => !_usedItemIds.contains(item.id))
-        .toList();
-    _currentSessionPool.shuffle(Random());
+    if (_usedItemIds.length >= allItems.length) _usedItemIds.clear();
+    _currentSessionPool = allItems.where((i) => !_usedItemIds.contains(i.id)).toList()..shuffle(Random());
   }
 
   QuizQuestion? getNextQuestion(int totalOptions) {
-    if (_currentSessionPool.isEmpty) {
-      _resetOrShufflePool();
+    if (_currentSessionPool.isEmpty) _resetOrShufflePool();
+    final target = _currentSessionPool.removeLast();
+    _usedItemIds.add(target.id);
+
+    List<String> opts = [target.word];
+    List<String> dist = List.from(target.distractors)..shuffle(Random());
+
+    for (var d in dist) {
+      if (opts.length < totalOptions && !opts.contains(d)) opts.add(d);
     }
+    opts.shuffle(Random());
 
-    final targetItem = _currentSessionPool.removeLast();
-    _usedItemIds.add(targetItem.id);
-
-    List<String> options = [targetItem.word];
-    List<String> availableDistractors = List.from(targetItem.distractors)..shuffle(Random());
-
-    for (var distractor in availableDistractors) {
-      if (options.length < totalOptions && !options.contains(distractor)) {
-        options.add(distractor);
-      }
-    }
-
-    options.shuffle(Random());
-
-    return QuizQuestion(
-      item: targetItem,
-      options: options,
-      correctAnswer: targetItem.word,
-    );
+    return QuizQuestion(item: target, options: opts, correctAnswer: target.word);
   }
 }
